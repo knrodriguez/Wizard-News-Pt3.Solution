805a2463c786f686f0ebabdf90e3de7602b6dfea
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-03-10 09:31:59 -0500

    Adding a Form to your views

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const client = require("../db");
 const postList = require("../views/postList");
 const postDetails = require("../views/postDetails");
+const addPost = require('../views/addPost');
 
 const baseQuery = "SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n";
 
@@ -12,6 +13,10 @@ router.get("/", async (req, res, next) => {
     const data = await client.query(baseQuery);
     res.send(postList(data.rows));
   } catch (error) { next(error) }
+});
+
+router.get("/add", (req, res) => {
+  res.send(addPost());
 });
 
 router.get("/:id", async (req, res) => {
